0dcb2909928ea4e75d430b20c55b33a9cee6ddb6
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-08-24 16:17:44 +0300

    finding standard deviation of randomly created datasets

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,6 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 fun enterNumber(min: Double = Double.MIN_VALUE, max: Double = Double.MAX_VALUE): Double {
     var number: Double? = null
     val prompt = if (min == Double.MIN_VALUE && max == Double.MAX_VALUE) {
@@ -26,7 +29,7 @@ fun enterNumber(min: Double = Double.MIN_VALUE, max: Double = Double.MAX_VALUE):
     return number
 }
 
-fun <T : Number> sum(vararg numbers: T): T {
+fun <T : Number> sum(numbers: List<T>): T {
 //    val total = numbers.fold(0) { acc, i -> acc + i }
     var total = 0.0
     for (number in numbers) {
@@ -35,7 +38,7 @@ fun <T : Number> sum(vararg numbers: T): T {
     return total as T
 }
 
-fun <T : Number> multiply(vararg numbers: T): T {
+fun <T : Number> multiply(numbers: List<T>): T {
     if (numbers.isEmpty()) {
         return 0.0 as T
     }
@@ -57,14 +60,55 @@ fun divide(numOne: Double, numTwo: Double): Double? {
     return numOne / numTwo
 }
 
+fun generateRandomArray(size: Int, min: Int = 0, max: Int = 100): List<Int> {
+    val numbers = mutableListOf<Int>()
+    if (size <= 0 || max < min) {
+        return numbers
+    }
+
+    for (index in 0..<size) {
+        val randomNumber: Int = (Math.random() * max).toInt() + min
+        numbers.add(index, randomNumber)
+    }
+    return numbers
+}
+
+/**
+ * calculates the standard deviation of a dataset.
+ * Standard Deviation = √(Σ(x - μ)² / N)
+ */
+fun standardDeviation(numbers: List<Int>): Double {
+    // step 1: calculate the mean
+    val mean: Double = sum(numbers).toDouble() / numbers.size.toDouble()
+
+    // step 2: find the difference between each data point and the mean.
+    // step 3: square each of the differences in step 2
+    val squareDiff: List<Double> = numbers.map{ num ->
+        (num - mean).pow(2.0)
+    }
+
+    // step 4: calculate the variance.
+    // add up all the squared differences. divide the sum by the total number of values
+    val variance: Double = sum(squareDiff) / squareDiff.size
+
+    // step 5: the square root of the variance is the standard deviation
+    return sqrt(variance)
+}
+
 fun main() {
-    val numberOne = enterNumber()
-    val numberTwo = enterNumber(0.0, 10.0)
+//    val numberOne = enterNumber()
+//    val numberTwo = enterNumber(0.0, 10.0)
+//    val twoNumbers = listOf(numberOne, numberTwo)
+//
+//    println("%.4f + %.4f = %.4f".format(numberOne, numberTwo, sum(twoNumbers)))
+//    println("%.4f * %.4f = %.4f".format(numberOne, numberTwo, multiply(twoNumbers)))
+//
+//    val divideResult = divide(numberOne, numberTwo)
+//    print("%.4f / %.4f = ".format(numberOne, numberTwo))
+//    if (divideResult == null) print("undefined") else print("%.4f".format(divideResult))
+//    println()
 
-    println("%.4f + %.4f = %.4f".format(numberOne, numberTwo, sum(numberOne, numberTwo)))
-    println("%.4f * %.4f = %.4f".format(numberOne, numberTwo, multiply(numberOne, numberTwo)))
-
-    val divideResult = divide(numberOne, numberTwo)
-    print("%.4f / %.4f = ".format(numberOne, numberTwo))
-    if (divideResult == null) print("undefined") else print("%.4f".format(divideResult))
+    val numbers: List<Int> = generateRandomArray(10)
+    val stdDeviation: Double = standardDeviation(numbers)
+    println("numbers: $numbers\nstandard deviation: %.2f".format(stdDeviation))
 }
