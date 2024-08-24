340494dc50b154e7551c4fd6ccd7ce14c4389349
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-08-24 07:54:32 +0300

    add, multiply, divide

--- src/Main.kt
+++ src/Main.kt
@@ -1,74 +1,63 @@
-import kotlin.system.exitProcess
-
-fun generateLuckyNumber(min: Int = 0, max: Int = 100): Int {
-    return (Math.random() * max).toInt() + min
-}
-
-fun playGame() {
-    val maxNum = 100
-    val minNum = 0
-    val luckyNumber = generateLuckyNumber(minNum, maxNum)
-    var userGuess: Int?
-    var numGuesses = 5
+fun enterNumber(min: Int = Int.MIN_VALUE, max: Int = Int.MAX_VALUE): Int {
+    var number: Int? = null
+    val prompt: String = if (min == Int.MIN_VALUE || max == Int.MAX_VALUE) {
+        "Enter a number: "
+    } else {
+        "Enter a number between $min and $max: "
+    }
 
     do {
-        if (numGuesses == 0) {
-            println("no more guesses left. You lose.")
-            println("the lucky number was $luckyNumber\n")
-            break
-        }
+        print(prompt)
+        val input: String = readlnOrNull() ?: continue
 
-        print("guess the lucky number between $minNum and $maxNum\n> ")
-        val userInput = readlnOrNull()
-        if (userInput == null) {
-            println("you chose not to play. Goodbye :/")
-            break
-        }
-
-        // validate userInput is an integer
-        userGuess = userInput.toIntOrNull()
-        if (userGuess == null) {
-            println("please select a number between $minNum and $maxNum")
+        number = input.toIntOrNull()
+        if (number == null) {
+            println("input is not a valid integer\n")
             continue
         }
 
-        // validate user's guess is between minNum and maxNum
-        if (userGuess < minNum || userGuess > maxNum) {
-            println("please select a number between $minNum and $maxNum")
-            continue
+        if (number < min || number > max) {
+            // number not within range
+            println("please enter a number between $min and $max\n")
+            number = null
         }
+    } while (number == null)
 
-        // hot or cold
-        if (userGuess > luckyNumber) {
-            println("too high")
-        } else if (userGuess < luckyNumber) {
-            println("too low")
-        } else {
-            println("you are right. the lucky number is indeed $luckyNumber")
-        }
+    return number
+}
 
-        numGuesses--
-        println("$numGuesses guesses left!\n")
-    } while (userGuess != luckyNumber)
+fun sum(vararg numbers: Int): Int {
+//    val total = numbers.fold(0) { acc, i -> acc + i}
+    var total = 0
+    for (number in numbers) {
+        total += number
+    }
+    return total
+}
+
+fun multiply(vararg numbers: Int): Int {
+//    val result = numbers.fold(1) { acc, i -> acc * i}
+    var result = 1
+    for (number in numbers) {
+        result *= number
+    }
+    return result
+}
+
+fun divide(numOne: Double, numTwo: Double): Double? {
+    if (numTwo == 0.0) {
+        // check divisor not zero. prevent divide by zero errors
+        return null
+    }
+
+    return numOne / numTwo
 }
 
 fun main() {
-    var playAgain = true
+    val numberOne = enterNumber()
+    val numberTwo = enterNumber(0, 10)
 
-    while (playAgain) {
-        playGame()
-
-        print("do you wish to play again? (y/n)\n> ")
-        val userInput = readlnOrNull() ?: run {
-            println("you chose not to play. Goodbye :/")
-            exitProcess(1)
-        }
-
-        playAgain = userInput.lowercase() == "y"
-        if (playAgain) {
-            println("\n") // add some spacing
-        }
-    }
-
-    println("exiting program. Goodbye")
+    println("$numberOne + $numberTwo = ${sum(numberOne, numberTwo)}")
+    println("$numberOne * $numberTwo = ${multiply(numberOne, numberTwo)}")
+    println("$numberOne / $numberTwo = ${divide(numberOne.toDouble(), numberTwo.toDouble()) ?: "undefined"}")
 }
