ff9272f9ae1a655dec5156b0d3b846fb3e3166a0
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-09-03 12:15:05 +0300

    fizz buzz

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
+
+
+fun fizzBuzz(number: Int): String {
+    val result = when {
+        number % 15 == 0 -> "FizzBuzz"
+        number % 3 == 0 -> "Fizz"
+        number % 5 == 0 -> "Buzz"
+        else -> "$number"
+    }
+    return result
+}
+
 fun main(args: Array<String>) {
-    println("Hello Kotlin")
+    for (number in 100 downTo 0 step 2) {
+        val result = fizzBuzz(number)
+        println(result)
+    }
 }
