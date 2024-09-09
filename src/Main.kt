fb8c8e1b28949b98d207af64787dbb922a8c1b51
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-09-09 13:08:45 +0300

    Kotlin Fundamentals Codelab - Internet profiles

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,44 @@
+import kotlin.system.exitProcess
 
+class Person(
+    val name: String,
+    age: Int,
+    hobby: String?=null,
+    val referrer: Person?=null
+) {
+    var age: Int = age
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            } else {
+                // preferably throw an IllegalArgumentException
+                println("invalid age argument: valid age must be between 0 and 100")
+                exitProcess(1)
+            }
+        }
+    var hobby: String? = hobby
+        get() {
+            return if (field == null) "Doesn't have a hobby" else "Likes to $field"
+        }
 
-fun fizzBuzz(number: Int): String {
-    val result = when {
-        number % 15 == 0 -> "FizzBuzz"
-        number % 3 == 0 -> "Fizz"
-        number % 5 == 0 -> "Buzz"
-        else -> "$number"
+    fun showProfile() {
+        val referrerMessage: String = if (referrer == null) {
+            "Doesn't have a referrer"
+        } else {
+            "Has a referrer named ${referrer.name}, who ${referrer.hobby?.lowercase()}"
+        }
+
+        println("Name: $name\n" +
+                "Age: $age\n" +
+                "Hobby: $hobby. $referrerMessage\n\n"
+        )
     }
-    return result
 }
 
-fun main(args: Array<String>) {
-    for (number in 100 downTo 0 step 2) {
-        val result = fizzBuzz(number)
-        println(result)
-    }
+fun main() {
+    val amanda = Person(name="Amanda", age=33, hobby="play tennis", referrer=null)
+    val atiqah = Person(name="Atiqah", age=28, hobby="climb", referrer=amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
 }
